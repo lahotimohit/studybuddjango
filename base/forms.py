4bff8b86ec090bd7e7da23086a0cd90d3ab87823
Mohit Lahoti <engtechno25@gmail.com>
2023-11-29 17:39:43 +0530

    Releasing new web app

---
+++ base/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room
+from .models import User
+
+
+class MyUserCreationForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password']
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
